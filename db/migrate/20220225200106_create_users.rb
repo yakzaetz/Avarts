bd69c09a9ab91758d89bb1ae15d1d2feb64678eb
yakzaetz <yakzaetz@gmail.com>
2022-02-26 20:53:43 -0500

    user auth

---
+++ db/migrate/20220225200106_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :gender
+      t.date :birthday
+      t.string :location
+      t.integer :weight
+      t.timestamps
+    end
+    add_index :users, :name
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
